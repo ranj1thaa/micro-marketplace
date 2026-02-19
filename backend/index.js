dadd2ba9c05c0697a616386077bd3820edd22da7
Ranj1thaa <r.17ranjitha@gmail.com>
2026-02-20 01:26:17 +0530

    Fixxed bugs

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express=require('express')
+const cors=require('cors')
+const app=express()
+const cookieParser = require('cookie-parser');
+const authRoute=require('../backend/routes/authRoute')
+const productRoute=require('../backend/routes/productRoute')
+app.use(express.json())
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://micro-marketplace-drab.vercel.app"
+  ],
+  credentials: true
+}));
+
+
+
+app.use(cookieParser());
+
+app.use('/auth', authRoute)
+app.use('/product', productRoute)
+
+app.use((err,req, res, next)=>{
+  const { status = 500, message = "Something went wrong" } = err
+  res.status(status).json({ message })
+})
+module.exports=app
