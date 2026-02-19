64190d7434275687e0a7812b8b52a55f55853863
Ranj1thaa <r.17ranjitha@gmail.com>
2026-02-20 00:51:40 +0530

    Fixxed bugs

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "https://micro-marketplace-odja.onrender.com/",
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true
 });
 
-console.log("Axios base URL:", import.meta.env.VITE_BASE_URL)
+console.log("Axios base URL:", import.meta.env.VITE_BASE_URL);
 export default api
